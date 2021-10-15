47878a6e597e17854b40eadb1bf4a3dd3294263d
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-10-15 04:07:02 +0200

    Removed unused class import statements.

--- src/ServerTrait.php
+++ src/ServerTrait.php
@@ -2,13 +2,7 @@
 
 namespace Lagdo\DbAdmin\Driver;
 
-use Lagdo\DbAdmin\Driver\Entity\ConfigEntity;
-use Lagdo\DbAdmin\Driver\Entity\TableEntity;
-use Lagdo\DbAdmin\Driver\Entity\ForeignKeyEntity;
-use Lagdo\DbAdmin\Driver\Entity\TriggerEntity;
 use Lagdo\DbAdmin\Driver\Entity\RoutineEntity;
-
-use Lagdo\DbAdmin\Driver\Db\ConnectionInterface;
 
 trait ServerTrait
 {
