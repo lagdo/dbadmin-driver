de4622e6f44f3921af804904dccf1d2141edc407
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-12-27 00:29:16 +0100

    Removed unused functions.

--- src/TranslatorInterface.php
+++ src/TranslatorInterface.php
@@ -14,22 +14,4 @@ interface TranslatorInterface
      * @return string
      */
     public function lang(string $idf, $number = null): string;
-
-    /**
-     * Format a decimal number
-     *
-     * @param int $number
-     *
-     * @return string
-     */
-    public function formatNumber(int $number): string;
-
-    /**
-     * Format elapsed time
-     *
-     * @param float $time Output of microtime(true)
-     *
-     * @return string
-     */
-    public function formatTime(float $time): string;
 }
